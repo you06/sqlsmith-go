eaaf514bd8bb541db109f08ff3e7e25e70950a5e
Felix <lls@felix.wen>
2019-10-26 17:05:18 +0800

    add sync

--- sqlsmith.go
+++ sqlsmith.go
@@ -1,39 +1,47 @@
 package sqlsmith
 
 import (
-	"github.com/pingcap/parser/ast"
 	"math/rand"
+	"sync"
 	"time"
+
+	"github.com/pingcap/parser/ast"
 )
 
 func New() *SQLSmith {
 	return &SQLSmith{
-		Rand: rand.New(rand.NewSource(time.Now().UnixNano())),
+		Rand:      rand.New(rand.NewSource(time.Now().UnixNano())),
 		Databases: make(map[string]*Database),
 	}
 }
 
 // SelectStmt make random select statement
-func (s *SQLSmith) SelectStmt (depth int) ast.Node {
+func (s *SQLSmith) SelectStmt(depth int) ast.Node {
 	s.depth = depth
 	return s.selectStmt(1)
 }
 
 func (s *SQLSmith) BatchGenSQL(n int) []string {
-	if n<=0{
+	if n <= 0 {
 		return []string{}
 	}
 	rand.Seed(time.Now().Unix())
-	result := make([]string, n)
-	for i:=0;i<n;i++{
+	var (
+		result = make([]string, n)
+		wg     = sync.WaitGroup{}
+	)
+	for i := 0; i < n; i++ {
+		wg.Add(1)
 		go func(idx int) {
+			defer wg.Done()
 			_ = s.constructSelectStmt(nil, rand.Int())
 			s, err := s.ToSQL()
-			if err!=nil{
+			if err != nil {
 				return
 			}
 			result[i] = s
 		}(i)
 	}
+	wg.Wait()
 	return result
 }
