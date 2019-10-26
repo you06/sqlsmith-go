86ac2c2c678d693accaa2b2a75ea71d34e020972
Felix <lls@felix.wen>
2019-10-26 17:03:45 +0800

    add gen sql

--- sqlsmith.go
+++ sqlsmith.go
@@ -18,3 +18,22 @@ func (s *SQLSmith) SelectStmt (depth int) ast.Node {
 	s.depth = depth
 	return s.selectStmt(1)
 }
+
+func (s *SQLSmith) BatchGenSQL(n int) []string {
+	if n<=0{
+		return []string{}
+	}
+	rand.Seed(time.Now().Unix())
+	result := make([]string, n)
+	for i:=0;i<n;i++{
+		go func(idx int) {
+			_ = s.constructSelectStmt(nil, rand.Int())
+			s, err := s.ToSQL()
+			if err!=nil{
+				return
+			}
+			result[i] = s
+		}(i)
+	}
+	return result
+}
