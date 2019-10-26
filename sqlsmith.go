96f4708caea97baa632a50d0a6b8e33418035320
FelixSeptem <whenhow94@gmail.com>
2019-10-26 17:20:05 +0800

    fix depth

--- sqlsmith.go
+++ sqlsmith.go
@@ -34,7 +34,7 @@ func (s *SQLSmith) BatchGenSQL(n int) []string {
 		wg.Add(1)
 		go func(idx int) {
 			defer wg.Done()
-			_ = s.constructSelectStmt(nil, rand.Int())
+			_ = s.constructSelectStmt(nil, rand.Intn(10))
 			s, err := s.ToSQL()
 			if err != nil {
 				return
